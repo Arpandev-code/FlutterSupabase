42b7973285fba6ceadc58670361930291f41d5fe
Arpandev-code <arpan.dev2016@gmail.com>
2023-12-24 22:18:23 +0530

    Project File

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final SupabaseClient supabase = Supabase.instance.client;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () async {
+              await supabase.auth.signOut();
+            },
+            icon: const Icon(Icons.logout),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text("Home page"),
+      ),
+    );
+  }
+}
